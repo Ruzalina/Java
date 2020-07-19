610721d9079eda174756bcbd2c5e22ace314e207
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-07-19 12:52:14 -0400

    Change commit

--- src/backAround.java
+++ src/backAround.java
@@ -7,3 +7,5 @@ public class backAround {
         }
     }
 
+//Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+
