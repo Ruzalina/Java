9fbb66bf427a1440a397c67446012dd715efbbed
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-07-19 12:33:49 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c ");
+        System.out.print("I love You! ");
+        System.out.println();
+        System.out.println("                          RUBAI");
+        System.out.println("Mnogo let razmyshlal ya nad...... ");
+        System.out.println("Neponatnogo net dla menya.....");
+        System.out.println("Mne izvestno, cho nichego....!");
+        System.out.println("                                      O.Hayam");
+        System.out.println("                                      Omar.Hayam");
+    }
+}
